580923a89c62440e0f08a9281347192bc47696d5
BrisaTielly <brisatielly12@gmail.com>
2025-01-19 22:32:29 -0300

    5

---
+++ src/hackerrank/DiagonalDifference.java
@@ -0,0 +1,20 @@
+package hackerrank;
+
+public class DiagonalDifference {
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int tam = arr.size();
+        int diagonalPrincipal = 0;
+        int diagonalInversa = 0;
+        int aux = tam - 1;
+        for(int i = 0; i<tam; i++){
+            for(int j = 0; j<tam; j++){
+                if(i == j) diagonalPrincipal += arr.get(i).get(j);
+                if(i+j == aux) diagonalInversa += arr.get(i).get(j);
+            }
+        }
+        System.out.println(diagonalPrincipal + " " + diagonalInversa);
+        int diagonalDifference = diagonalPrincipal - diagonalInversa;
+        return (diagonalDifference < 0) ? Math.abs(diagonalDifference) : diagonalDifference;
+
+    }
+}
