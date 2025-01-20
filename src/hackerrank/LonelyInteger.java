8cbac61bfecce0e1be173d7d21b514b1ad88312d
BrisaTielly <brisatielly12@gmail.com>
2025-01-19 22:07:10 -0300

    4

---
+++ src/hackerrank/LonelyInteger.java
@@ -0,0 +1,19 @@
+package hackerrank;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LonelyInteger {
+    public static int lonelyinteger(List<Integer> a) {
+        Set<Integer> set = new HashSet<>();
+        for(Integer num : a){
+            if(set.contains(num)) {
+                set.remove(num);
+            }else {
+                set.add(num);
+            }
+        }
+        return set.iterator().next();
+    }
+}
